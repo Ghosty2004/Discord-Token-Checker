1a941302c75ad5d2c8aebef310406def83d04425
Ghosty2004 <72819231+Ghosty2004@users.noreply.github.com>
2021-10-18 19:25:01 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,14 @@
+# Discord Token Checker
+
+# How to use ?
+1. **npm install**.
+2. Put tokens in **tokens.txt** file.
+3. Start the app.
+4. Wait for results 
+
+# To know 
+- The <font color="red">red</font> color means that it is not valid, and the <font color="green">green</font> color means that it is valid.
+- When it reaches the end, it will show you how many valid tokens have been found.
+
+# Image
+<img src="https://i.imgur.com/cZ8edFQ.png">
---
+++ index.js
@@ -0,0 +1,59 @@
+const axios = require("axios");
+const fs = require("fs");
+require("colors");
+
+const token = {
+    Validate: function(token) {
+        return new Promise(async (resolve, reject) => {
+            axios.default.post(`https://discord.com/api/v6/invite/${getRandomInt(1, 9999999)}`, {}, {
+                headers: { 
+                    "Authorization": token 
+                }
+            }).then((response) => {
+                if(response.data.message == "401: Unauthorized") {
+                    resolve(false);
+                }
+                else {
+                    resolve(true);
+                }
+            }).catch((error) => {
+                if(error.response.data.message == "401: Unauthorized") {
+                    resolve(false);
+                }
+                else {
+                    resolve(true);
+                }
+            });
+        });
+    }
+}
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+}
+
+fs.readFile("tokens.txt", 'utf8', async function(err, data) {
+    if(!err) {
+        data = data.replace(/\r/g, "");
+        let array = data.split("\n");
+        let good_tokens = 0;
+
+        console.log("\n");
+        console.log("Token checher has started...");
+
+        for(let i = 0; i < array.length; i++) {
+            if(await token.Validate(array[i])) {
+                console.log(`[TOKEN]: `.green + `${array[i]}`);
+                good_tokens++;
+            }
+            else {
+                console.log(`[TOKEN]: `.red + `${array[i]}`);
+            }
+        }
+
+        console.log("Found " + `${good_tokens}`.yellow + " valid tokens.");
+        console.log("\n");
+    }
+});
